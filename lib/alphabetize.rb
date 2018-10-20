e7f1f4927196534e7d6e2bc9c9d1fe0d86518a8b
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-20 05:25:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,9 @@ def alphabetize(arr)
   # code here
   arr.sort_by do |esperanto_alphabet|
     #binding.pry
-    esperanto_alphabet.chars
-    binding.pry
+    esperanto_alphabet.chars.map do |i|
+      binding.pry 
+    end
+    #binding.pry
   end
 end
