c034ca0f8605503fafc4272cd7bf271526eb2310
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-20 05:04:23 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,10 @@
+require 'pry'
 def alphabetize(arr)
   # code here
   e_a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |esperanton_phrase|
     esperanton_phrase.chars.map do |i|
+      binding.pry
       e_a.chars.index(i)
     end
   end
